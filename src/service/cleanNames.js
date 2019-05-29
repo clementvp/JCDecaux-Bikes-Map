152520aa58b65567c5097c3eb3dc6e69404a2578
Van Peuter Clément <clement.vanpeuter@gmail.com>
2019-05-29 13:43:28 +0200

    change the cleanStationsName to fit all the contracts in France (#5)

--- src/service/cleanNames.js
+++ src/service/cleanNames.js
@@ -1,7 +1,7 @@
 const cleanStationsNames = (stations) => {
   stations.map((station) => {
     const newStation = station;
-    newStation.name = station.name.replace('#', '').substring(5).replace('-', '').trim();
+    newStation.name = station.name.replace('#', '').replace(/\d*/i, '').replace('-', '').trim();
     return newStation;
   });
   return stations;
