8bcb30af2e07d3e078e681c77d2ef35f55dc0ba1
Van Peuter Clément <clement.vanpeuter@gmail.com>
2019-06-11 13:07:55 +0200

    Translate the stations status to french (#12)

--- src/service/cleanStationsData.js
+++ src/service/cleanStationsData.js
@@ -1,9 +1,14 @@
-const cleanStationsNames = (stations) => {
+const cleanStationsData = (stations) => {
   stations.map((station) => {
     const newStation = station;
+    if (station.status === 'OPEN') {
+      newStation.status = 'OUVERTE';
+    } else {
+      newStation.status = 'FERMÉE';
+    }
     newStation.name = station.name.replace('#', '').replace(/\d*/i, '').replace('-', '').trim();
     return newStation;
   });
   return stations;
 };
-export default cleanStationsNames;
+export default cleanStationsData;
