932c527d638d1e224f862e01c7f38d4fa7886f49
clement <clement.vanpeuter@zenika.com>
2019-06-11 13:06:46 +0200

    Translate the stations status to french

--- src/service/getJCDecauxData.js
+++ src/service/getJCDecauxData.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
-import cleanStationsNames from './cleanNames';
+import cleanStationsData from './cleanStationsData';
 
 const getJCDecauxData = () => new Promise(async (resolve, reject) => {
   try {
     const response = await axios.get(`https://api.jcdecaux.com/vls/v1/stations?contract=${process.env.VUE_APP_JCDECAUX_CITY}&apiKey=${process.env.VUE_APP_JCDECAUX_API_KEY}`);
-    const data = cleanStationsNames(response.data);
+    const data = cleanStationsData(response.data);
     resolve(data);
   } catch (error) {
     reject(error);
